d0b1b6cf032d6c515f871717038d2538c2a95ea0
Saroj Rai <raisaroj360@gmail.com>
2025-06-26 07:41:58 -0400

    feat(kokoro): add GPU support and performance monitoring

--- server/app/services/kokoro/kokoro.py
+++ server/app/services/kokoro/kokoro.py
@@ -6,6 +6,7 @@ wget https://github.com/thewh1teagle/kokoro-onnx/releases/download/model-files-v
 python examples/save.py
 """
 
+import time
 from typing import Optional
 from kokoro_onnx import Kokoro
 from pydantic import BaseModel
@@ -20,15 +21,28 @@ class KokoroGenerationConfig(BaseModel):
 
 class KokoroService:
     def __init__(self) -> None:
-        self.kokoro = Kokoro(
-            "app/services/kokoro/kokoro-v1.0.onnx",
-            "app/services/kokoro/voices-v1.0.bin"
-        )
+        try:
+            self.kokoro = Kokoro(
+                "app/services/kokoro/kokoro-v1.0.onnx",
+                "app/services/kokoro/voices-v1.0.bin",
+                providers=['CUDAExecutionProvider']
+            )
+            print(f"Loaded Kokoro model with CUDA")
+        except Exception as e:
+            self.kokoro = Kokoro(
+                "app/services/kokoro/kokoro-v1.0.onnx",
+                "app/services/kokoro/voices-v1.0.bin",
+            )
+            print(f"Loaded Kokoro model with CPU")
 
     def generate_audio(self, output_path: str, config: KokoroGenerationConfig):        
         print(f"Generating audio with Kokoro: {config.model_dump(mode='json')}")
+        start_time = time.time()
         samples, sample_rate = self.kokoro.create(config.text, voice=config.voice, speed=config.speed, lang=config.lang)
         sf.write(output_path, samples, sample_rate)
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print(f"Generated audio in {elapsed_time:.2f} seconds")
 
     @staticmethod
     def get_voices() -> list[str]:
